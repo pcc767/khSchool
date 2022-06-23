c7f0cf1458e229b7a4793ba922251d6ea931cdd9
Kooki <pcc767@gmail.com>
2022-06-24 00:25:06 +0900

    0624_javascript_Array_Function

---
+++ dev-source_Front/JS/JS06_HTML태그접근1.js
@@ -0,0 +1,10 @@
+// window.addEventListener('load', function() {
+//     var div1 = document.querySelector('#div1');
+//     var div1_h2 = div1.querySelector('#div1_h2');
+//     var div1_p1 = div1.querySelector('#div1_p1');
+
+//     div1_h2.innerHTML = '<h3 style="color:red;">새로 바뀐 h3입니다.</h3>';
+//     div1_p1.innerText = '새로 바뀐 p 태그 입니다'
+
+
+// });
